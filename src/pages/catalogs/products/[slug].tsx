fc3c6e766e4d14e828cfbe17ef6725249688fe94
allexis <allexissoares@gmail.com>
2020-12-24 21:01:45 -0300

    feat: connect page products to prismic api

--- src/pages/catalogs/products/[slug].tsx
+++ src/pages/catalogs/products/[slug].tsx
@@ -1,12 +1,53 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Link from 'next/link';
+import PrismicDOM from 'prismic-dom';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import Prismic from 'prismic-javascript';
+import { Document } from 'prismic-javascript/types/documents';
 
-export default function Product() {
+import { client } from '@/lib/prismic';
+interface ProductProps {
+  product: Document;
+}
+
+export default function Product({ product }: ProductProps) {
   const router = useRouter();
+
+  if (router.isFallback) {
+    return <p>Loading...</p>
+  }
 
   return (
     <div>
-      <h1>{router.query.slug}</h1>
+      <h1>
+      {PrismicDOM.RichText.asText(product.data.title)}
+      </h1>
+
+      <img src={product.data.thumbnail.url} width={300} alt={product.data.url}/>
+
+      <div dangerouslySetInnerHTML={{ __html: PrismicDOM.RichText.asHtml(product.data.description) }} />
+
+      <p>Preço: R${product.data.price}</p>
     </div>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  }
+};
+
+export const getStaticProps: GetStaticProps<ProductProps> = async (context) => {
+  const { slug } = context.params;
+
+  const product = await client().getByUID('product', String(slug), {});
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 5,
+  }
 }
